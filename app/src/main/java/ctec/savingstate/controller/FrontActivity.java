f83bc561d0047d4418268e14ee43af29501583fa
Colm Laro <smartman97@gmail.com>
2015-10-14 08:33:16 -0600

    An app working with state saving

---
+++ app/src/main/java/ctec/savingstate/controller/FrontActivity.java
@@ -0,0 +1,13 @@
+package ctec.savingstate.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FrontActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front);
+    }
+}
